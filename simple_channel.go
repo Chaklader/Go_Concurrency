d614095df880d9e58560f30cc4ac08e27b5df35e
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-05-12 19:25:21 +0600

    go concurrency code

---
+++ simple_channel.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <-ping
+
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func simpleChannel() {
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Println("Type something and press ENTER (enter Q to quit)")
+
+	for {
+		fmt.Print("-> ")
+
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if userInput == strings.ToLower("q") {
+			// jump out of for loop
+			break
+		}
+
+		ping <- userInput
+
+		response := <-pong
+
+		fmt.Println("Response:", response)
+	}
+
+	fmt.Println("All done. Closing channels.")
+
+	// close the channels
+	close(ping)
+	close(pong)
+}
